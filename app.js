b712432b50b834f8abfaaa14bdf80bf36c6d3e64
Donna <dlambertpdx@gmail.com>
2018-02-26 16:57:52 -0800

    updated js page

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
+$(() =>{
+
 const API_KEY = 'AIzaSyCBxwI_1JYyOQ0a5pi8ETEoYo1GXHOFVBM';
 const URL = 'https://www.googleapis.com/youtube/v3/search';
 
-function getDataFromApi(searchTerm, callback){
+function callApi(searchTerm, callback){
   const settings = {
     url: URL,
-    part: 'snippet',
     key: API_KEY,
-    q:searchTerm
+    part: 'snippet',
+    q:searchTerm,
+    maxResults: 6
   };
 }
+
+function renderResult(result) {
+// return html and string literals for results
+}
+
+
+function displayResults(obj) {
+// add val to results on html page
+}
+
+
+function handleResults(data) {
+// remove hidden class from results
+}
+
+
+function watchSubmit() {
+// what happens when submit button is clicked
+}
+
+
+watchSubmit();
+});
