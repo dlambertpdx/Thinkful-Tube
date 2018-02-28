c5704b6fe4a60a23fde329d403e0cfe1b9a281ca
Donna <dlambertpdx@gmail.com>
2018-02-27 17:00:46 -0800

    updated js file

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
-$(() =>{
-
 const API_KEY = 'AIzaSyCBxwI_1JYyOQ0a5pi8ETEoYo1GXHOFVBM';
 const URL = 'https://www.googleapis.com/youtube/v3/search';
 
-function callApi(searchTerm, callback){
-  const settings = {
-    url: URL,
-    key: API_KEY,
+
+function callApi(searchTerm, pageToken, callback){
+  const query = {
+    type: 'video',
+    q: searchTerm,
+    maxResults: 3,
     part: 'snippet',
-    q:searchTerm,
-    maxResults: 6
+    key: API_KEY,
+    pageToken: pageToken
   };
+
+  $.getJSON(URL, query, callback)
 }
+
+function  watchSubmit() {
+  $('form').submit((e) => {
+    e.preventDefault();
+    const queryTarget = $(e.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    callApi(query, undefined, displayResults);
+  });
+}
+
+function watchPageToken() {
+  $('.js-search-results').on('click', '#nextPage', (e) => {
+    callApi(searchTerm, nextPage, displayResults)
+  });
+  $('.js-search-results').on('click', '#prevPage', (e) => {
+    callApi(searchTerm, prevPage, displayResults)
+  });
+}
+
+function renderPageNav(data){
+  nextPage = data.nextPageToken;
+  prevPage = data.prevPageToken;
+  let startTag = `<div class='pageNav'><form><fieldset>`
+  let endTag = `</fieldset></form></div>`
+  if (prevPage) {
+      startTag += `<button type='button' id='prevPage' value='Prev'>Prev</button>`
+    }
+    if (nextPage) {
+      startTag += `<button type='button' id='nextPage' value='Next'>Next</button>`
+    }
+    return startTag + endTag
+  }
 
 function renderResult(result) {
-// return html and string literals for results
+  return(`<div class='result'>
+    <a href='https://www.youtube.com/watch?v=${result.id.videoId}' target='_blank' class= 'thumbnail' role='link'>
+    <img src='${result.snippet.thumbnails.medium.url}' alt='video thumbnail'></a>
+    <p class='caption'>${result.snippet.title}</p></div>`)
 }
 
-
-function displayResults(obj) {
-// add val to results on html page
+function displayResults(data) {
+  $('.js-search-results').html(data.items.map(renderResult)).append(renderPageNav(data));
+  console.log(data)
 }
-
-
-function handleResults(data) {
-// remove hidden class from results
-}
-
-
-function watchSubmit() {
-// what happens when submit button is clicked
-}
-
-
-watchSubmit();
-});
+$(watchSubmit);
+$(watchPageToken);
